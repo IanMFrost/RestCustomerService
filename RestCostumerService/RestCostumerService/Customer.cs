240b018a09c68a58d3c6d87b9720b33ea98be649
IanMFrost <ianmfrost0@gmail.com>
2018-03-21 12:25:33 +0100

    rest service.

---
+++ RestCostumerService/RestCostumerService/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestCostumerService
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Year { get; set; }
+
+
+        public Customer(int id, string first, string last, int year)
+        {
+            ID = id;
+            FirstName = first;
+            LastName = last;
+            Year = year;
+            
+        }
+
+        public Customer()
+        {
+
+        }
+
+
+
+    }
+}
